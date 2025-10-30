e95bb70800df095c944289fae746f2aeb67e072d
danielantmann <tula3x3@gmail.com>
2025-10-30 11:39:44 +0100

    Feat/buttons (#1)
    * feat(btn): create CustomButtonComponent and styles
    
    * feat(btn) : add doubleSize custom prop, to manage btns sizes
    
    * feat(calculator): install and configure heptics dependencies

---
+++ components/CustomCalculatorButton.tsx
@@ -0,0 +1,42 @@
+import { Colors } from "@/constants/Colors";
+import { globalStyles } from "@/styles/global-styles";
+import * as Haptics from "expo-haptics";
+import React from "react";
+import { Pressable, Text } from "react-native";
+interface Props {
+  label: string;
+  color?: string;
+  blackText?: boolean;
+  doubleSize?: boolean;
+  onPress: () => void;
+}
+const CustomCalculatorButton = ({
+  label,
+  color = Colors.darkGrey,
+  blackText = false,
+  doubleSize = false,
+  onPress,
+}: Props) => {
+  const textColor = blackText ? Colors.blackText : Colors.textPrimary;
+
+  return (
+    <Pressable
+      style={({ pressed }) => ({
+        ...globalStyles.button,
+        backgroundColor: color,
+        opacity: pressed ? 0.8 : 1,
+        width: doubleSize ? 180 : 80,
+      })}
+      onPress={() => {
+        Haptics.impactAsync();
+        onPress();
+      }}
+    >
+      <Text style={[globalStyles.buttonText, { color: textColor }]}>
+        {label}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default CustomCalculatorButton;
